d947c5ad2f10a0457a719f63530ac8250fb69f15
stuart lamble <lamble.stuart@googlemail.com>
2020-03-29 15:21:32 +0100

    Added navbar & footer components, added routes to App component

---
+++ profile/public/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+    return (
+
+        <nav className="navbar fixed-bottom" 
+        style="background-color: #666666; color: white; text-align: 
+        center; border-top: 8px solid #4aaaa5"> 
+        Copyright &#9400;
+        </nav>
+    );
+
+    }
+
+export default Footer
--- profile/public/src/App.js
+++ profile/public/src/App.js
@@ -1,6 +1,24 @@
 import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import About from "./components/pages/About";
+import Portfolio from "./components/pages/Portfolio";
+import Contact from "./components/pages/Contact";
 
-const App = () => <Profile />;
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Route exact path="/" component={About} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route path="/contact" component={Contact} />
+        <Footer />
+      </div>
+    </Router>
+  );
+}
 
 export default App;
